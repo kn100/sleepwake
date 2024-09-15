b5c25e6ce401cc01da83d0e854ebea65628dfc9f
kn100 <kn100@kn100.me>
2024-09-15 00:33:06 -0400

    make it actually work

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # sleepwake
-some garbage to make my pc do stuff when it goes into and out of suspension.
+Some garbage to make my pc do stuff when it goes into and out of suspension.
+
+If you too need your PC to do stuff when it goes to sleep, and then do other
+more different stuff when it wakes up, you can define whatever code you like
+in the `onSleep()` and `onWake()` funcs, make the binary, copy it somewhere 
+(I suggest /opt), and then add a systemd unit for it. Start the service, et
+voila, you too get my garbage.
+
+Your tasks should take less than 5 seconds, since that's how long you get 
+by default from Systemd to do whatever it is you need to do before the system
+goes to sleep. If you need longer, look into configuring `InhibitDelayMaxSec`
+(logind.conf).
+
+
 
 Example systemd unit
 ```
--- main.go
+++ main.go
@@ -14,66 +14,84 @@ import (
 
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
-	var fd int
-	var conn *dbus.Conn
+
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		log.Fatal().Err(err).Msg("error connecting to dbus")
+	}
 	defer conn.Close()
 	for {
-		if conn == nil || !conn.Connected() {
-			var err error
-			conn, err = dbus.SystemBus()
-			if err != nil {
-				log.Error().Err(err).Msg("error connecting to dbus")
-				break
-			}
-		}
-		var err error
-		if fd == 0 || syscall.FcntlFlock(uintptr(fd), syscall.F_SETLKW, &syscall.Flock_t{Type: syscall.F_WRLCK}) != nil {
-			fd, err = RegisterSleepListener(conn)
-		}
-
+		fd, err := InhibitSleep(conn)
 		if err != nil {
-			log.Error().Err(err).Msg("error registering sleep listener")
+			log.Fatal().Err(err).Msg("error inhibiting sleep")
+		}
+		log.Info().Msg("inhibited sleep, waiting for system to sleep")
+
+		err = WaitForSleep(conn)
+		if err != nil {
+			log.Fatal().Err(err).Msg("error waiting for sleep")
 			break
 		}
+		log.Info().Msg("system is sleeping shortly")
 
-		dbusChan := make(chan *dbus.Signal, 10)
-		conn.Signal(dbusChan)
-		log.Info().Int("fd", fd).Msg("waiting for sleep")
-		for sig := range dbusChan {
-			if sig.Name == "org.freedesktop.login1.Manager.PrepareForSleep" {
-				prepareForSleep, ok := sig.Body[0].(bool)
-				if ok && !prepareForSleep {
-					err := switchTo("pc")
-					if err != nil {
-						log.Error().Err(err).Msg("error waking up")
-					}
-				} else if ok && prepareForSleep {
-					err := switchTo("mac")
-					if err != nil {
-						log.Error().Err(err).Msg("error suspending")
-					}
-				} else {
-					log.Error().Msg("error parsing signal")
-				}
-			}
-			_, err := syscall.Seek(fd, 0, 0)
-			if err != nil {
-				// Inability to seek means descriptor is closed, so
-				// we break so the whole thing can restart.
-				break
-			}
-			// Just in case it isn't closed, we close it
-			err = syscall.Close(fd)
-			if err != nil {
-				log.Error().Err(err).Msg("error closing file descriptor")
-			}
+		err = onSleep()
+		if err != nil {
+			log.Error().Err(err).Msg("error doing sleep tasks")
 		}
+		log.Info().Msg("done with sleep tasks")
+
+		sleepTime := time.Now()
+		err = UninhibitSleep(fd)
+		if err != nil {
+			log.Error().Err(err).Msg("error uninhibiting sleep")
+			break
+		}
+		log.Info().Msg("giving up our sleep lock to allow system to sleep")
+		// At some point, the system is going to go to sleep. We don't know exactly
+		// when that will be, but program execution will stop at some point. After,
+		// it continues as if nothing happened. We need to detect that. We do this
+		// by seeing if 15 wall clock seconds have passed since the time we slept
+		// If it has, we assume the system has woken up and we can continue going.
+		for time.Since(sleepTime) < 15*time.Second {
+			time.Sleep(100 * time.Millisecond)
+		}
+		log.Info().Msg("15 wall clock seconds have passed since last sleep event, assuming we're awake")
+
+		err = OnWake()
+		if err != nil {
+			log.Error().Err(err).Msg("error switching to pc")
+		}
+		log.Info().Msg("done with wake tasks")
 	}
 	log.Info().Msg("exiting")
 }
 
-func RegisterSleepListener(conn *dbus.Conn) (int, error) {
-	var fd int
+// Blocks until a sleep is detected
+func WaitForSleep(dbusConn *dbus.Conn) error {
+	err := dbusConn.AddMatchSignal(
+		dbus.WithMatchInterface("org.freedesktop.login1.Manager"),
+		dbus.WithMatchObjectPath("/org/freedesktop/login1"),
+		dbus.WithMatchMember("PrepareForSleep"),
+	)
+	if err != nil {
+		return err
+	}
+	dbusChan := make(chan *dbus.Signal, 10)
+	dbusConn.Signal(dbusChan)
+	for sig := range dbusChan {
+		if sig.Name == "org.freedesktop.login1.Manager.PrepareForSleep" {
+			prepareForSleep, ok := sig.Body[0].(bool)
+			if ok && prepareForSleep {
+				return nil
+			}
+		}
+	}
+	return fmt.Errorf("error waiting for sleep")
+}
+
+// Returns a fd which represents a lock on the system's ability to sleep
+func InhibitSleep(conn *dbus.Conn) (uint32, error) {
+	var fd uint32
 	err := conn.Object(
 		"org.freedesktop.login1",
 		dbus.ObjectPath("/org/freedesktop/login1"),
@@ -82,23 +100,34 @@ func RegisterSleepListener(conn *dbus.Conn) (int, error) {
 		0,
 		"sleep",
 		"sleepwake",
-		"Do stuff on suspend",
+		"sleepwake needs time to do stuff",
 		"delay",
 	).Store(&fd)
 	if err != nil {
-		return -1, err
+		return uint32(0), err
 	}
-
-	err = conn.AddMatchSignal(
-		dbus.WithMatchInterface("org.freedesktop.login1.Manager"),
-		dbus.WithMatchObjectPath("/org/freedesktop/login1"),
-		dbus.WithMatchMember("PrepareForSleep"),
-	)
-	if err != nil {
-		return -1, err
-	}
-
 	return fd, nil
+}
+
+// Uninhibits sleep by closing the file descriptor
+func UninhibitSleep(fd uint32) error {
+	err := syscall.Close(int(fd))
+	if err != nil {
+		return fmt.Errorf("error closing file descriptor: %w", err)
+	}
+	return nil
+}
+
+// Called when the system is going to sleep
+// This is where you put your sleep tasks
+func onSleep() error {
+	return switchTo("mac")
+}
+
+// Called when the system has woken up
+// This is where you put your wake tasks
+func OnWake() error {
+	return switchTo("pc")
 }
 
 func switchTo(system string) error {
@@ -108,9 +137,9 @@ func switchTo(system string) error {
 	}
 	defer resp.Body.Close()
 	if resp.Status == "409" {
-		log.Info().Str("requested", system).Msg("already in requested mode")
+		log.Info().Str("switched_to", system).Msg("already in requested mode")
 	} else {
-		log.Info().Str("requested", system).Msg("switched over")
+		log.Info().Str("switched_to", system).Msg("switched over")
 	}
 	return nil
 }
@@ -127,6 +156,5 @@ func makeRequestWithRetry(url string, retries int) (*http.Response, error) {
 		}
 		return resp, nil
 	}
-	log.Error().Err(err).Msg("request failed")
-	return resp, fmt.Errorf("failed to make request after %d retries", retries)
+	return resp, fmt.Errorf("failed to make request after %d retries. Last error: %s", retries, err.Error())
 }
